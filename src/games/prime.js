f71093e5038e83520826b203fbbcf5760e83bc88
Alexander <alexxgranovski@gmail.com>
2019-09-02 19:15:28 +0300

    correction of names, refactoring

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,12 +2,12 @@ import generationNumber from '../utils';
 import playGame from '..';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const isPrime = (num) => {
-  if (num < 2) {
+const isPrime = (number) => {
+  if (number < 2) {
     return false;
   }
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0 && num > 2) {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0 && number > 2) {
       return false;
     }
   }
